01a546c1148daa02ee42b3fe9dd85480e3fccd19
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-07-09 02:25:22 +0530

    Update index.ts

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -23,7 +23,7 @@ app.use(passport.authenticate('session'));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://multiplayer-chess-platform-frontend.vercel.app",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
