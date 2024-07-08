1ffc67c57f4e5a4c9a2ce51a311a17e1e12bbf2c
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-07-09 02:27:27 +0530

    Update useSocket.ts

--- apps/frontend/src/hooks/useSocket.ts
+++ apps/frontend/src/hooks/useSocket.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { useUser } from "@repo/store/useUser";
 
-const WS_URL = "ws://localhost:8080";
+const WS_URL = "https://multiplayer-chess-platform-websocket.vercel.app";
 
 export const useSocket = () => {
     const [socket, setSocket] = useState<WebSocket | null>(null);
