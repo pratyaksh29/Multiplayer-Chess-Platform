2300f00fed172c8f9b1e7c1c122acc5417e4b454
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2024-07-09 11:25:00 +0530

    Update index.ts

--- apps/ws/src/index.ts
+++ apps/ws/src/index.ts
@@ -4,20 +4,18 @@ import url from "url";
 import { extractUserId } from './auth';
 import { User } from './SocketManager';
 
-const wss = new WebSocketServer({ port: 8080 });
-
+const port = process.env.PORT || 8080;
+const wss = new WebSocketServer({ port });
 const gameManager = new GameManager();
 
 wss.on('connection', function connection(ws, req) {
-
   //@ts-ignore
   const token: string = url.parse(req.url, true).query.token;
   const userId = extractUserId(token);
   gameManager.addUser(new User(ws, userId));
-
   ws.on("close", () => {
     gameManager.removeUser(ws)
   })
 });
 
-console.log("done")
+console.log(`WebSocket server is running on port ${port}`);
